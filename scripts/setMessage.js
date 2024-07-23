41fb27648652923360486c48bf5ef0d16d26702c
lokiboss1 <85043556+lokiboss1@users.noreply.github.com>
2024-07-23 15:31:37 +0800

    Add files via upload

---
+++ scripts/setMessage.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpclink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpclink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x0b8A4EffAA103dAe9B9c7f259b08ACAC873B5C47";
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "setMessage";
+  const messageToSet = "Hello Swisstronik!!";
+  const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData(functionName, [messageToSet]), 0);
+  await setMessageTx.wait();
+  console.log("Transaction Receipt: ", setMessageTx);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
